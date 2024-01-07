8f51e97982a76161a07cf40687b1ab4ef9ac2f5e
Jeffrey Morris <ffejcsirrom@gmail.com>
2024-01-07 17:44:59 -0600

    fix BuildConstants bug

---
+++ src/main/java/io/github/tigerbotics7125/BuildConstants.java
@@ -0,0 +1,19 @@
+package io.github.tigerbotics7125;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+    public static final String MAVEN_GROUP = "";
+    public static final String MAVEN_NAME = "FRC2024";
+    public static final String VERSION = "unspecified";
+    public static final int GIT_REVISION = 3;
+    public static final String GIT_SHA = "b5c9b896cc1d20dcb33bdcf1b4b02c2442b04f6c";
+    public static final String GIT_DATE = "2024-01-07 17:35:57 CST";
+    public static final String GIT_BRANCH = "main";
+    public static final String BUILD_DATE = "2024-01-07 17:44:16 CST";
+    public static final long BUILD_UNIX_TIME = 1704671056280L;
+    public static final int DIRTY = 1;
+
+    private BuildConstants(){}
+}
