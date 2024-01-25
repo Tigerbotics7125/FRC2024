32836a44378c28ca7dd3378efc39904919b9b574
sbeneze <sbeneze@users.noreply.github.com>
2024-01-25 17:46:20 +0000

    [bot] Lint

--- src/main/java/io/github/tigerbotics7125/Robot.java
+++ src/main/java/io/github/tigerbotics7125/Robot.java
@@ -8,33 +8,22 @@ package io.github.tigerbotics7125;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.drive.RobotDriveBase.MotorType;
 import edu.wpi.first.wpilibj.motorcontrol.PWMTalonSRX;
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-import io.github.tigerbotics7125.Constants.OperatorConstants;
-import io.github.tigerbotics7125.commands.Autos;
 import io.github.tigerbotics7125.commands.ExampleCommand;
 import io.github.tigerbotics7125.subsystems.ExampleSubsystem;
 
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.revrobotics.CANSparkMax;
-
-//import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+// import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 /**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the
- * name of this class or
- * the package after creating this project, you must also update the
- * build.gradle file in the
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
  * project.
  */
 public class Robot extends TimedRobot {
-    
 
     // private RobotContainer m_robotContainer;
 
@@ -42,26 +31,23 @@ public class Robot extends TimedRobot {
     private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
     // Additional controllers may be added if needed.
-    
 
     private PWMTalonSRX leftMotor = new PWMTalonSRX(1);
     private PWMTalonSRX rightMotor = new PWMTalonSRX(2);
     private DifferentialDrive mDrive = new DifferentialDrive(leftMotor, rightMotor);
-      private XboxController mXbox = new XboxController(0);
-
+    private XboxController mXbox = new XboxController(0);
 
     /*
      * private CANSparkMax mLeft1 = new CANSparkMax(0, MotorType.kBrushed);
      * private CANSparkMax mLeft2 = new CANSparkMax(1, MotorType.kBrushed);
      * private CANSparkMax mRight1 = new CANSparkMax(2, MotorType.kBrushed);
      * private CANSparkMax mRight2 = new CANSparkMax(3, MotorType.kBrushed);
-     * 
+     *
      * private DifferentialDrive mDrive = new DifferentialDrive(mLeft1, mRight1);
      * private XboxController mXbox = new XboxController(0);
      */
     /**
-     * This function is run when the robot is first started up and should be used
-     * for any
+     * This function is run when the robot is first started up and should be used for any
      * initialization code.
      */
     @Override
@@ -71,17 +57,12 @@ public class Robot extends TimedRobot {
     }
 
     /**
-     * Use this method to define your trigger->command mappings. Triggers can be
-     * created via the
-     * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
-     * an arbitrary
+     * Use this method to define your trigger->command mappings. Triggers can be created via the
+     * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
      * predicate, or via the named factories in {@link
-     * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
-     * {@link
-     * CommandXboxController
-     * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-     * PS4} controllers or
-     * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+     * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
+     * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+     * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
      * joysticks}.
      */
     private void configureBindings() {
@@ -92,17 +73,14 @@ public class Robot extends TimedRobot {
         // Schedule 'exampleMethodCommand' when the Xbox controller's B button is
         // pressed,
         // cancelling on release.
-        
+
     }
 
     /**
-     * This function is called every 20 ms, no matter the mode. Use this for items
-     * like diagnostics
+     * This function is called every 20 ms, no matter the mode. Use this for items like diagnostics
      * that you want ran during disabled, autonomous, teleoperated and test.
      *
-     * <p>
-     * This runs after the mode specific periodic functions, but before LiveWindow
-     * and
+     * <p>This runs after the mode specific periodic functions, but before LiveWindow and
      * SmartDashboard integrated updating.
      */
     @Override
@@ -114,35 +92,30 @@ public class Robot extends TimedRobot {
         // and running subsystem periodic() methods. This must be called from the
         // robot's periodic
         // block in order for anything in the Command-based framework to work.
-        
+
     }
 
     /** This function is called once each time the robot enters Disabled mode. */
     @Override
-    public void disabledInit() {
-    }
+    public void disabledInit() {}
 
     @Override
-    public void disabledPeriodic() {
-    }
+    public void disabledPeriodic() {}
 
     /**
-     * This autonomous runs the autonomous command selected by your
-     * {@link RobotContainer} class.
+     * This autonomous runs the autonomous command selected by your {@link RobotContainer} class.
      */
     @Override
     public void autonomousInit() {
         // An example command will be run in autonomous
-        
 
         // schedule the autonomous command (example)
-        
+
     }
 
     /** This function is called periodically during autonomous. */
     @Override
-    public void autonomousPeriodic() {
-    }
+    public void autonomousPeriodic() {}
 
     @Override
     public void teleopInit() {
@@ -150,14 +123,13 @@ public class Robot extends TimedRobot {
         // teleop starts running. If you want the autonomous to
         // continue until interrupted by another command, remove
         // this line or comment it out.
-        
+
     }
-    
+
     /** This function is called periodically during operator control. */
     @Override
     public void teleopPeriodic() {
         mDrive.arcadeDrive(mXbox.getLeftY(), mXbox.getLeftX(), false);
-
     }
 
     @Override
@@ -168,16 +140,13 @@ public class Robot extends TimedRobot {
 
     /** This function is called periodically during test mode. */
     @Override
-    public void testPeriodic() {
-    }
+    public void testPeriodic() {}
 
     /** This function is called once when the robot is first started up. */
     @Override
-    public void simulationInit() {
-    }
+    public void simulationInit() {}
 
     /** This function is called periodically whilst in simulation. */
     @Override
-    public void simulationPeriodic() {
-    }
+    public void simulationPeriodic() {}
 }
