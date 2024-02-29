73ff35b5b2c62b521897ad94a25e36ff8a797efd
Jeffrey Morris <ffejcsirrom@gmail.com>
2024-02-29 17:03:34 -0600

    created TimedAutonomous class

---
+++ src/main/java/io/github/tigerbotics7125/TimedAutonomous.java
@@ -0,0 +1,75 @@
+package io.github.tigerbotics7125;
+
+import com.revrobotics.RelativeEncoder;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class TimedAutonomous {
+   String autonomous1 = "Autonomous 1";
+    String autonomous2 = "Autonomous 2";
+    String autonomousSelect;
+    double startTime = Timer.getFPGATimestamp();
+    double howLong = 5;
+    double howLong2 = 10;
+    double deltaTime;
+    SendableChooser<String> m_chooserAutonomous = new SendableChooser<>();
+
+    public TimedAutonomous(RelativeEncoder leftMEncoder, RelativeEncoder rightMEncoder){
+    
+        startTime = Timer.getFPGATimestamp();
+        
+        m_chooserAutonomous.setDefaultOption("Autonomous 1", autonomous1);
+        m_chooserAutonomous.addOption("Autonomous 2", autonomous2);
+        SmartDashboard.putData("Autonomous", m_chooserAutonomous);
+        
+        
+   } 
+   public void autoChooser(DifferentialDrive mDrive){
+    
+
+        autonomousSelect = m_chooserAutonomous.getSelected();
+        
+
+
+        switch (autonomousSelect) {
+            case "Autonomous 1":
+                runAutoLeft(mDrive);
+                break;
+
+            case "Autonomous 2":
+                runAutoRight(mDrive);
+                break;
+
+            default:
+                break;
+        }
+   }
+   public void runAutoLeft(DifferentialDrive mDrive){
+    deltaTime = Timer.getFPGATimestamp() - startTime;
+    if(deltaTime < howLong){
+        mDrive.tankDrive(0.5, 0);
+    }
+    else if (deltaTime < howLong2){
+        mDrive.tankDrive(0.5, 0.5);
+    }
+    else{
+        mDrive.tankDrive(0, 0);
+    }
+   }
+
+   public void runAutoRight(DifferentialDrive mDrive){
+    deltaTime = Timer.getFPGATimestamp() - startTime;
+    if(deltaTime < howLong){
+        mDrive.tankDrive(0, 0.5);
+    }
+    else if (deltaTime < howLong2){
+        mDrive.tankDrive(0.5, 0.5);
+    }
+    else{
+        mDrive.tankDrive(0, 0);
+    }
+   }
+}
