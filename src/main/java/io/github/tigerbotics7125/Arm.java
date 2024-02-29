e3401ea5636a6bfb665b51a07a997bc8ea149cf1
Jeffrey Morris <ffejcsirrom@gmail.com>
2024-02-28 18:21:44 -0600

    PID attempt

--- src/main/java/io/github/tigerbotics7125/Arm.java
+++ src/main/java/io/github/tigerbotics7125/Arm.java
@@ -12,53 +12,70 @@ import com.revrobotics.SparkPIDController;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Arm {
-    private CANSparkMax armMotor1;
-    private CANSparkMax armMotor2;
-
-    private RelativeEncoder armEncoder;
-    private final double downAngle = 0;
-    private final double ampAngle = 180;
-    private final double shootingAngle = 90;
-
-    private SparkPIDController mPID;
+    private static final int deviceID = 8;
+    private CANSparkMax m_motor1;
+    private CANSparkMax m_motor2;
+    private SparkPIDController m_pidController;
+    private RelativeEncoder m_encoder;
     public double kP, kI, kD, kIz, kFF, kMaxOutput, kMinOutput;
 
     public Arm(int armMotor1ID, int armMotor2ID) {
-        armMotor1 = new CANSparkMax(armMotor1ID, MotorType.kBrushless);
-        armMotor2 = new CANSparkMax(armMotor2ID, MotorType.kBrushless);
-        armMotor2.follow(armMotor1);
-        armEncoder = armMotor1.getEncoder();
-        mPID = armMotor1.getPIDController();
+        // initialize motor
+        m_motor1 = new CANSparkMax(armMotor1ID, MotorType.kBrushless);
+        m_motor2 = new CANSparkMax(armMotor2ID, MotorType.kBrushless);
+
+        /**
+         * The restoreFactoryDefaults method can be used to reset the configuration
+         * parameters
+         * in the SPARK MAX to their factory default state. If no argument is passed,
+         * these
+         * parameters will not persist between power cycles
+         */
+        m_motor1.restoreFactoryDefaults();
+        m_motor2.restoreFactoryDefaults();
+        m_motor2.follow(m_motor1,true);
+
+        /**
+         * In order to use PID functionality for a controller, a SparkPIDController
+         * object
+         * is constructed by calling the getPIDController() method on an existing
+         * CANSparkMax object
+         */
+        m_pidController = m_motor1.getPIDController();
+
+        // Encoder object created to display position values
+        m_encoder = m_motor1.getEncoder();
 
         // PID coefficients
-        kP = 0.1;
-        kI = 1e-4;
-        kD = 1;
+        kP = 0.04;
+        kI = 1e-7;
+        kD = .5;
         kIz = 0;
         kFF = 0;
         kMaxOutput = 1;
         kMinOutput = -1;
 
         // set PID coefficients
-        mPID.setP(kP);
-        mPID.setI(kI);
-        mPID.setD(kD);
-        mPID.setIZone(kIz);
-        mPID.setFF(kFF);
-        mPID.setOutputRange(kMinOutput, kMaxOutput);
+        m_pidController.setP(kP);
+        m_pidController.setI(kI);
+        m_pidController.setD(kD);
+        m_pidController.setIZone(kIz);
+        m_pidController.setFF(kFF);
+        m_pidController.setOutputRange(kMinOutput, kMaxOutput);
 
         // display PID coefficients on SmartDashboard
-        SmartDashboard.putNumber("Arm P Gain", kP);
-        SmartDashboard.putNumber("Arm I Gain", kI);
-        SmartDashboard.putNumber("Arm D Gain", kD);
-        SmartDashboard.putNumber("Arm I Zone", kIz);
-        SmartDashboard.putNumber("Arm Feed Forward", kFF);
-        SmartDashboard.putNumber("Arm Max Output", kMaxOutput);
-        SmartDashboard.putNumber("Arm Min Output", kMinOutput);
-        SmartDashboard.putNumber("Arm Set Rotations", 0);
+        SmartDashboard.putNumber("P Gain", kP);
+        SmartDashboard.putNumber("I Gain", kI);
+        SmartDashboard.putNumber("D Gain", kD);
+        SmartDashboard.putNumber("I Zone", kIz);
+        SmartDashboard.putNumber("Feed Forward", kFF);
+        SmartDashboard.putNumber("Max Output", kMaxOutput);
+        SmartDashboard.putNumber("Min Output", kMinOutput);
+        SmartDashboard.putNumber("Set Rotations", 0);
     }
 
     public void teleop() {
+        // read PID coefficients from SmartDashboard
         double p = SmartDashboard.getNumber("P Gain", 0);
         double i = SmartDashboard.getNumber("I Gain", 0);
         double d = SmartDashboard.getNumber("D Gain", 0);
@@ -66,80 +83,53 @@ public class Arm {
         double ff = SmartDashboard.getNumber("Feed Forward", 0);
         double max = SmartDashboard.getNumber("Max Output", 0);
         double min = SmartDashboard.getNumber("Min Output", 0);
+        double rotations = SmartDashboard.getNumber("Set Rotations", 0);
 
         // if PID coefficients on SmartDashboard have changed, write new values to
         // controller
         if ((p != kP)) {
-            mPID.setP(p);
+            m_pidController.setP(p);
             kP = p;
         }
         if ((i != kI)) {
-            mPID.setI(i);
+            m_pidController.setI(i);
             kI = i;
         }
         if ((d != kD)) {
-            mPID.setD(d);
+            m_pidController.setD(d);
             kD = d;
         }
         if ((iz != kIz)) {
-            mPID.setIZone(iz);
+            m_pidController.setIZone(iz);
             kIz = iz;
         }
         if ((ff != kFF)) {
-            mPID.setFF(ff);
+            m_pidController.setFF(ff);
             kFF = ff;
         }
         if ((max != kMaxOutput) || (min != kMinOutput)) {
-            mPID.setOutputRange(min, max);
+            m_pidController.setOutputRange(min, max);
             kMinOutput = min;
             kMaxOutput = max;
         }
-    }
 
-    public void moveToPostion(double setPostion) {
-        double rotations = SmartDashboard.getNumber("Arm Set Rotations", 0);
-        System.out.println("Rotations: " + rotations);
         /**
-         * PIDController objects are commanded to a set point using the SetReference() method.
-         *
-         * <p>The first parameter is the value of the set point, whose units vary depending on the
-         * control type set in the second parameter.
-         *
-         * <p>The second parameter is the control type can be set to one of four parameters:
+         * PIDController objects are commanded to a set point using the
+         * SetReference() method.
+         * 
+         * The first parameter is the value of the set point, whose units vary
+         * depending on the control type set in the second parameter.
+         * 
+         * The second parameter is the control type can be set to one of four
+         * parameters:
          * com.revrobotics.CANSparkMax.ControlType.kDutyCycle
          * com.revrobotics.CANSparkMax.ControlType.kPosition
          * com.revrobotics.CANSparkMax.ControlType.kVelocity
          * com.revrobotics.CANSparkMax.ControlType.kVoltage
          */
-        mPID.setReference(rotations, CANSparkMax.ControlType.kPosition);
+        m_pidController.setReference(rotations, CANSparkMax.ControlType.kPosition);
 
-        SmartDashboard.putNumber("Arm SetPoint", rotations);
-        SmartDashboard.putNumber("Arm ProcessVariable", armEncoder.getPosition());
-    }
-
-    public void raiseArm() {
-
-        armMotor1.set(.25);
-    }
-
-    public void lowerArm() {
-
-        armMotor1.set(-.25);
-    }
-
-    public void stopArm() {
-        mPID.setReference(armEncoder.getPosition(), CANSparkMax.ControlType.kPosition);
-    }
-
-    public void amp() {
-        moveToPostion(ampAngle);
-    }
-
-    public void shoot() {
-        moveToPostion(shootingAngle);
-    }
-
-    public void stow() {
-        moveToPostion(downAngle);
+        SmartDashboard.putNumber("SetPoint", rotations);
+        SmartDashboard.putNumber("ProcessVariable", m_encoder.getPosition());
     }
 }
